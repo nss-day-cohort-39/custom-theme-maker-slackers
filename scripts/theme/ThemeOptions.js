82313f441e3e59203afdab2786f04784d1a127df
Andrew Harper <druharper@gmail.com>
2020-03-13 13:06:15 -0500

    change backgd colors js

--- scripts/theme/ThemeOptions.js
+++ scripts/theme/ThemeOptions.js
@@ -1,13 +1,23 @@
+import { BorderButtons } from "./BorderButtons.js"
 import { ColorButtons } from "./ColorButtons.js"
 import { FontButtons } from "./FontButtons.js"
-import { BorderButtons } from "./BorderButtons.js"
 
 export const ThemeOptions = () => {
-    return `
-        <article class="container__panel themeOptions">
-            ${BorderButtons()}
-            ${ColorButtons()}
-            ${FontButtons()}
-        </article>
-    `
+  return `
+    <article class="container__panel themeOptions">
+      ${BorderButtons()}
+      ${ColorButtons()}
+      ${FontButtons()}
+    </article>
+  `
 }
+
+// >>>>>>>>>>>>>>>>>>>
+const eventHub = document.querySelector("#container")
+
+eventHub.addEventListener("colorSelected", event => {
+  const color = event.detail.color
+
+  eventHub.classList = []
+  eventHub.classList.add(color)
+})
